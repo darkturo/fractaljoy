f0138a3bbad9733fd456107af26053c446c9d6af
Arturo Escudero <darkturo@gmail.com>
2013-12-24 05:28:40 +0100

    Updated script to print a basic formula
    Up to this point, this is how I think the formula should be. We'll see
    later on which shape it finally takes.

--- src/formula-skel-gen.py
+++ src/formula-skel-gen.py
@@ -1,39 +1,50 @@
+import os
 import os.path
 import argparse
-
+import datetime
 
 def main():
     description = """Lua formula skeleton generator."""
     parser = argparse.ArgumentParser(prog=os.path.basename(__file__), 
                                      description = description)
     parser.add_argument('-v', '--version', action = 'version', version = "%(prog)s 0.1.alfa")
-    parser.add_argument('-f', choices = genFormulaSkel.keys(), required = True, help='select the formula skeleton or template to be generated.')
+    formulaTypes = ['base-motif', 'clusters', 'canopies', 'complex', 'IFS']
+    parser.add_argument('-f', choices = formulaTypes, required = True, help='select the formula skeleton or template to be generated.')
     options = parser.parse_args()
-    genFormulaSkel[options.f]()
+    print genFormulaTemplate(options.f)
 
-def genBaseMotifFormula():
-    pass
+def genFormulaTemplate(formulaType):
+    header = """
+require "fractaljoy"
+--[[
+title: Descriptive title for this formula
+author: {0:s}
+date: {1:s}
+description: Formula description
+]]
 
-def genClusterFormula():
-    pass
+function formulaType()
+    return "{2:s}"
+end
 
-def genCanopiesFormula():
-    pass
+--[[ Customizable parameters hash map. ]]
+formulaParameters = {{}}
 
-def genComplexFormula():
-    pass
+function retrieveFormulaParameters()
+    --[[ get Formula customizable parameters if any. ]]
+    return formulaParameters
+end 
 
-def genIFSFormula():
-    pass
-
-# fractaljoy formula types:
-genFormulaSkel = {
-    'base-motif':  genBaseMotifFormula,
-    'clusters': genClusterFormula,
-    'canopies': genCanopiesFormula,
-    'complex': genComplexFormula,
-    'IFS': genIFSFormula
-}
+function runFormula(params)
+    --[[ Lua algorithm to generate the fractal. It will receive params with the
+         options set by the user using the fractaljoy interface. 
+         This function will use the primitives drawPoint(), drawLine or drawPoligon() and flush
+         provided by fractaljoy.primitives
+    ]]
+    params = params or {{}}
+end
+    """.format(os.environ["USER"], datetime.date.today().strftime("%Y-%m-%d"), formulaType)    
+    return header
 
 if (__name__ == "__main__"):
     main()
