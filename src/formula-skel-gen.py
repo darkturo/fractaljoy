994115aaa965305ff2e9c70e2dab433c1101c822
Arturo Escudero <darkturo@gmail.com>
2013-12-24 04:04:45 +0100

    Extended the skeleton of the script.
    Not sure though if this is going to be the final look

--- src/formula-skel-gen.py
+++ src/formula-skel-gen.py
@@ -2,10 +2,38 @@ import os.path
 import argparse
 
 
-if (__name__ == "__main__"):
+def main():
     description = """Lua formula skeleton generator."""
     parser = argparse.ArgumentParser(prog=os.path.basename(__file__), 
                                      description = description)
     parser.add_argument('-v', '--version', action = 'version', version = "%(prog)s 0.1.alfa")
-    parser.add_argument('-f', choices = ['base-motif', 'clusters', 'canopies', 'complex', 'IFS'], required = True, help='select the formula skeleton or template to be generated.')
+    parser.add_argument('-f', choices = genFormulaSkel.keys(), required = True, help='select the formula skeleton or template to be generated.')
     options = parser.parse_args()
+    genFormulaSkel[options.f]()
+
+def genBaseMotifFormula():
+    pass
+
+def genClusterFormula():
+    pass
+
+def genCanopiesFormula():
+    pass
+
+def genComplexFormula():
+    pass
+
+def genIFSFormula():
+    pass
+
+# fractaljoy formula types:
+genFormulaSkel = {
+    'base-motif':  genBaseMotifFormula,
+    'clusters': genClusterFormula,
+    'canopies': genCanopiesFormula,
+    'complex': genComplexFormula,
+    'IFS': genIFSFormula
+}
+
+if (__name__ == "__main__"):
+    main()
